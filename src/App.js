4cd617111430a690845167ccbbe4990ef48afd7b
a18245107 <AOSherstnev@sberbank.ru>
2021-01-02 00:27:05 +0300

    add eslint

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import bridge from '@vkontakte/vk-bridge';
 import View from '@vkontakte/vkui/dist/components/View/View';
-import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
 import '@vkontakte/vkui/dist/vkui.css';
 
 import Lending from './panels/lending';
 
-
 const App = () => {
-	const [activePanel, setActivePanel] = useState('lending');
-	const [fetchedUser, setUser] = useState(null);
-	const [popout, setPopout] = useState(null);
+  const [activePanel, setActivePanel] = useState('lending');
+  const [fetchedUser, setUser] = useState(null);
+  const [popout, setPopout] = useState(null);
 
-	useEffect(() => {
-		bridge.subscribe(({ detail: { type, data }}) => {
-			if (type === 'VKWebAppUpdateConfig') {
-				const schemeAttribute = document.createAttribute('scheme');
-				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
-				document.body.attributes.setNamedItem(schemeAttribute);
-			}
-		});
-		async function fetchData() {
-			const user = await bridge.send('VKWebAppGetUserInfo');
-			setUser(user);
-			setPopout(null);
-		}
-		fetchData();
-	}, []);
+  useEffect(() => {
+    bridge.subscribe(({ detail: { type, data } }) => {
+      if (type === 'VKWebAppUpdateConfig') {
+        const schemeAttribute = document.createAttribute('scheme');
+        schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
+        document.body.attributes.setNamedItem(schemeAttribute);
+      }
+    });
+    async function fetchData() {
+      const user = await bridge.send('VKWebAppGetUserInfo');
+      setUser(user);
+      setPopout(null);
+    }
+    fetchData();
+  }, []);
 
-	const go = e => {
-		setActivePanel(e.currentTarget.dataset.to);
-	};
+  const go = e => {
+    setActivePanel(e.currentTarget.dataset.to);
+  };
 
-	return (
-		<View activePanel={activePanel} popout={popout}>
-			<Lending id='lending' fetchedUser={fetchedUser} go={go} />
-		</View>
-	);
-}
+  return (
+    <View activePanel={activePanel} popout={popout}>
+      <Lending id="lending" fetchedUser={fetchedUser} go={go} />
+    </View>
+  );
+};
 
 export default App;
-
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import "core-js/features/map";
-import "core-js/features/set";
-import React from "react";
-import ReactDOM from "react-dom";
-import connect from "@vkontakte/vk-connect";
+import 'core-js/features/map';
+import 'core-js/features/set';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import connect from '@vkontakte/vk-connect';
 
-import App from "./App";
+import App from './App';
 
 // Init VK  Mini App
-connect.send("VKWebAppInit",{});
+connect.send('VKWebAppInit', {});
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
+ReactDOM.render(<App />, document.getElementById('root'));
